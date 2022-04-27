896eef5e3457bf4316b50b509f69790c0e174e3a
Mariyan Yordanov <marsrewq@gmail.com>
2022-04-27 17:22:26 +0300

    Merge

--- ForLoops/ExerciseForLoops/5.Salary.js
+++ ForLoops/ExerciseForLoops/5.Salary.js
@@ -1,31 +1,31 @@
-function salary(token){
-    let openTabs = Number(token[0]);
-    let salary = Number(token[1]);
-    for (let index = 2; index <= token.length; index++) {
-        let tab = token[index];
-        if (tab === "Facebook") {
-            salary -= 150;
-        } else if (tab === "Instagram") {
-            salary -= 100;
-        } else if (tab === "Reddit") {
-            salary -= 50;
-        }
+// function salary(token){
+//     let salary = Number(token[1]);
+//     for (let index = 2; index <= token.length; index++) {
+//         let tab = token[index];
+//         if (tab === "Facebook") {
+//             salary -= 150;
+//         } else if (tab === "Instagram") {
+//             salary -= 100;
+//         } else if (tab === "Reddit") {
+//             salary -= 50;
+//         }
 
-        if (salary <= 0) {
-            console.log(`You have lost your salary.`);
-            return;
-        } 
-    }
+//         if (salary <= 0) {
+//             console.log(`You have lost your salary.`);
+//             return;
+//         } 
+//     }
 
-    console.log(Math.round(salary));
-}
+//     console.log(Math.round(salary));
+// }
 
-salary(["10",
-"750",
-"Facebook",
-"Dev.bg",
-"Instagram",
-"Facebook",
-"Reddit",
-"Facebook",
-"Facebook"]);
+// salary(["10",
+// "750",
+// "Facebook",
+// "Dev.bg",
+// "Instagram",
+// "Facebook",
+// "Reddit",
+// "Facebook",
+// "Facebook"]);
+
