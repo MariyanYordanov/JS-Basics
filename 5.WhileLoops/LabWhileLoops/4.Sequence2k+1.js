64140f5c2c2190da15d5229d43d004bea101f55d
Mariyan Yordanov <marsrewq@gmail.com>
2022-04-29 19:43:35 +0300

    Fix

--- 5.WhileLoops/LabWhileLoops/4.Sequence2k+1.js
+++ 5.WhileLoops/LabWhileLoops/4.Sequence2k+1.js
@@ -1,13 +1,10 @@
 function sequence(token){
     let number = Number(token[0]);
     let index = 1;
-    console.log(index);
-    let newNumber = index * 2 + 1;
-    while (newNumber <= number ) {
-        console.log(newNumber);
-        index++;
-        newNumber = index * 2 + 1;
+    while (index <= number ) {
+        console.log(index);
+        index =index * 2 + 1;
     }
 }
 
-sequence(["8"]);
+sequence(["31"]);
