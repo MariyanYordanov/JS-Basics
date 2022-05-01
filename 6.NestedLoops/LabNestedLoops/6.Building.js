3f14b7fe786ac9f562ef6bf7832af7069c98f957
Mariyan Yordanov <marsrewq@gmail.com>
2022-05-02 01:09:53 +0300

    Fix

--- 6.NestedLoops/LabNestedLoops/6.Building.js
+++ 6.NestedLoops/LabNestedLoops/6.Building.js
@@ -1,7 +1,21 @@
-function building(token){
-    let floorNumber = Number(token[0]);
-    let roomsNumber = Number(token[1]);
-    
+function building(input) {
+    let maxFloors = Number(input.shift());
+    let maxRooms = Number(input.shift());
+ 
+    for (let floor = maxFloors; floor > 0; floor--) {
+        let floorString = "";
+        for (let room = 0; room < maxRooms; room++) {
+            if (floor == maxFloors) {
+                floorString += "L" + floor + room + " ";
+            } else if (floor % 2 == 0) {
+                floorString += "O" + floor + room + " ";
+            } else {
+                floorString += "A" + floor + room + " ";
+            }
+        }
+        
+        console.log(floorString);
+    }
 }
 
 building(["6","4"]);
