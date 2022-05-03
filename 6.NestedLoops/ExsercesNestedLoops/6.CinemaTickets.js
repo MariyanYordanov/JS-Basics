561d70d795162c11b4953230bd05b2a4cc3fc605
Mariyan Yordanov <marsrewq@gmail.com>
2022-05-03 18:18:53 +0300

    Fix

--- 6.NestedLoops/ExsercesNestedLoops/6.CinemaTickets.js
+++ 6.NestedLoops/ExsercesNestedLoops/6.CinemaTickets.js
@@ -1,50 +1,35 @@
-function cinemaTickets(token){
-    let command = token.shift();
-    let totalTickets = 0;
-    let studentTotal = 0;
-    let standardTotal = 0;
-    let kidTotal = 0;
-    while (command !== "Finish") {
-        let movietitle = command;
-        let freeSeats = Number(token.shift());
-        let studentType = 0;
-        let standardType = 0;
-        let kidType = 0;
-        let takenSeats = 0;
-        let ticketType = token.shift();
-        while (ticketType !== "End" ) {
-            if ( freeSeats === takenSeats ) {
+function cinemaTicket(input) {
+    let nameMovie = input.shift();
+    let totalTicket = 0;
+    let studentCounter = 0;
+    let standardCounter = 0;
+    let kidsCounter = 0;
+    while (nameMovie !== 'Finish') {
+        let freeSpace = Number(input.shift());
+        let typeTicket = input.shift();
+        let sellsTicket = 0;
+        while (typeTicket !== 'End') {
+            if (typeTicket === 'student') {
+                studentCounter++;
+            } else if (typeTicket === 'standard') {
+                standardCounter++;
+            } else {
+                kidsCounter++;
+            }
+            totalTicket++;
+            sellsTicket++;
+            if (sellsTicket >= freeSpace) {
                 break;
             }
-
-            if (ticketType === "student") {
-                studentType++;
-                studentTotal++;
-            } else if (ticketType === "standard") {
-                standardType++;
-                standardTotal++;
-            } else if (ticketType === "kid") {
-                kidType++;
-                kidTotal++;
-            }
-            totalTickets++;
-            takenSeats++;
-            ticketType = token.shift();
+            typeTicket = input.shift();
         }
-        let percentageFullHall = takenSeats / freeSeats * 100;
-        console.log(`${movietitle} - ${percentageFullHall.toFixed(2)}% full.`);
-        if(ticketType === "Finish"){
-            break;
-        }
-        command = token.shift();
+        console.log(`${nameMovie} - ${((sellsTicket / freeSpace) * 100).toFixed(2)}% full.`);
+        nameMovie = input.shift();
     }
-    let percentageStudentTickets = studentTotal / totalTickets * 100;
-    let percentageStandardTickets = standardTotal / totalTickets * 100;
-    let percentageKidTickets = kidTotal / totalTickets * 100;
-    console.log(`Total tickets: ${totalTickets}`);
-    console.log(`${percentageStudentTickets.toFixed(2)}% student tickets.`);
-    console.log(`${percentageStandardTickets.toFixed(2)}% standard tickets.`);
-    console.log(`${percentageKidTickets.toFixed(2)}% kids tickets.`);
+    console.log(`Total tickets: ${totalTicket}`);
+    console.log(`${((studentCounter / totalTicket) * 100).toFixed(2)}% student tickets.`);
+    console.log(`${((standardCounter / totalTicket) * 100).toFixed(2)}% standard tickets.`);
+    console.log(`${((kidsCounter / totalTicket) * 100).toFixed(2)}% kids tickets.`);
 }
 
 cinemaTickets(["The Matrix",
